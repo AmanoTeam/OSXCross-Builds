34dbf27a9714bac9357f81ea2575af96341636be
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-09-24 16:23:57 -0300

    Initial commit

---
+++ main.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -eu
+
+declare -r workdir="${PWD}"
+
+declare -r toolchain_directory='/tmp/darwin'
+declare -r share_directory="${toolchain_directory}/usr/local/share/darwin"
+
+declare -r darwin_tarball='/tmp/darwin.tar.xz'
+declare -r darwin_url='https://github.com/AmanoTeam/OSXCross-Builds/releases/latest/download/x86_64-unknown-linux-gnu.tar.xz'
+
+sudo \
+	apt-get \
+	--assume-yes \
+	install \
+	lsb-release \
+	wget \
+	software-properties-common \
+	gnupg \
+	build-essential
+
+cd '/tmp'
+
+wget 'https://apt.llvm.org/llvm.sh'
+sudo bash './llvm.sh' '22'
+
+for old in '/usr/bin/'*'-22'; do
+	declare new="$(sed 's/-22//g' <<< "${old}")"
+	sudo unlink "${new}" 2>/dev/null || true
+	sudo ln --symbolic "${old}" "${new}"
+done
+
+if [ "${1}" = 'build' ]; then
+	git \
+		clone \
+		--branch 'testing' \
+		--depth '1' \
+		'https://github.com/tpoechtrager/osxcross'
+	
+	cd 'osxcross'
+	
+	wget \
+		'https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz' \
+		--directory-prefix="${PWD}/tarballs"
+	
+	TARGET_DIR='/tmp/darwin' UNATTENDED='1' ./build.sh
+	
+	mkdir --parent "${share_directory}"
+	
+	cp \
+		--recursive \
+		"${workdir}/tools/dev/"* \
+		"${share_directory}"
+	
+	[ -d "${toolchain_directory}/build" ] || mkdir "${toolchain_directory}/build"
+	
+	ln \
+		--symbolic \
+		--relative \
+		"${share_directory}/"* \
+		"${toolchain_directory}/build"
+else
+	curl \
+		--url "${darwin_url}" \
+		--retry '30' \
+		--retry-delay '0' \
+		--retry-all-errors \
+		--retry-max-time '0' \
+		--location \
+		--silent \
+		--output "${darwin_tarball}"
+	
+	tar \
+		--directory="$(dirname "${toolchain_directory}")" \
+		--extract \
+		--file="${darwin_tarball}"
+fi
---
+++ tools/dev/autotools/x86_64h-unknown-apple-darwin.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+kopt="${-}"
+
+set -eu
+
+CROSS_COMPILE_TRIPLET='x86_64-apple-darwin'
+CROSS_COMPILE_SYSTEM='darwin'
+CROSS_COMPILE_ARCHITECTURE='x86_64'
+CROSS_COMPILE_SYSROOT='/tmp/darwin/SDK/MacOSX11.3.sdk'
+
+CC='/tmp/darwin/bin/x86_64h-apple-darwin20.4-clang'
+CXX='/tmp/darwin/bin/x86_64h-apple-darwin20.4-clang++'
+AR='/usr/bin/llvm-ar'
+AS='/usr/bin/llvm-as'
+LD='/tmp/darwin/bin/x86_64h-apple-darwin20.4-ld'
+NM='/usr/bin/llvm-nm'
+RANLIB='/usr/bin/llvm-ranlib'
+STRIP='/usr/bin/llvm-strip'
+OBJCOPY='/usr/bin/llvm-objcopy'
+READELF='/usr/bin/llvm-readelf'
+
+export \
+	CROSS_COMPILE_TRIPLET \
+	CROSS_COMPILE_SYSTEM \
+	CROSS_COMPILE_ARCHITECTURE \
+	CROSS_COMPILE_SYSROOT \
+	CC \
+	CXX \
+	AR \
+	AS \
+	LD \
+	NM \
+	RANLIB \
+	STRIP \
+	OBJCOPY \
+	READELF
+
+[[ "${kopt}" = *e*  ]] || set +e
+[[ "${kopt}" = *u*  ]] || set +u

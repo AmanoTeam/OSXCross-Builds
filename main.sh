4b350675606093a1a7c808eb332069445632a0ea
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-09-24 16:39:50 -0300

    Add missing include for macOS

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 declare -r toolchain_directory='/tmp/darwin'
 declare -r share_directory="${toolchain_directory}/usr/local/share/darwin"
 
@@ -17,6 +19,12 @@ cd '/tmp'
 
 wget 'https://apt.llvm.org/llvm.sh'
 sudo bash './llvm.sh' '22'
+
+for old in '/usr/bin/'*'-22'; do
+	declare new="$(sed 's/-22//g' <<< "${old}")"
+	sudo unlink "${new}" 2>/dev/null || true
+	sudo ln --symbolic "${old}" "${new}"
+done
 
 if [ "${1}" = 'build' ]; then
 	git \
